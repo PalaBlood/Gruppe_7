779c314d453da1277992df5600872fd3fa252761
Halil0927 <halilx2000@hotmail.de>
2024-05-07 22:58:46 +0200

    Admin class create_user methode

--- src/server/HalilsTaverneAdministration.py
+++ src/server/HalilsTaverneAdministration.py
@@ -11,5 +11,22 @@ from .bo.Fridge import Fridge
 class HalilsTaverneAdministration (object):
 
     def __init__(self):
-            print("test")
+            pass
+    
+    #User spezifische Methoden
+
+    def create_user(self, nick_name, first_name, last_name, google_user_id):
+          #Benutzer erstellen
+          user = User()
+          user.set_first_name(first_name)
+          user.set_last_name(last_name)
+          user.set_nickname(nick_name)
+          user.set_google_id(google_user_id)
+          user.set_id(1) #default id, wird später in der Datenbank geupdatet
+
+          with UserMapper() as mapper:
+                return mapper.insert(user)
+          
+
+
         
