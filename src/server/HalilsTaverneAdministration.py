9786fc9211206d8d6f973c497c455b8bef97e6b1
Halil0927 <halilx2000@hotmail.de>
2024-05-22 10:24:35 +0200

    Mapper class

--- src/server/HalilsTaverneAdministration.py
+++ src/server/HalilsTaverneAdministration.py
@@ -13,8 +13,10 @@ class HalilsTaverneAdministration(object):
 
     def __init__(self):
             pass
+    """
+    User spezifische Methoden
     
-    #User spezifische Methoden
+    """
 
     def create_user(self, nick_name, first_name, last_name, google_user_id):
           #Benutzer erstellen
@@ -28,20 +30,51 @@ class HalilsTaverneAdministration(object):
           with UserMapper() as mapper:
                 return mapper.insert(user)
           
-    #müssen noch richtig implementiert werden
-
-    def get_all_users(self):
+    #Suchen eines Users
+    def get_user(self, id):
         with UserMapper() as mapper:
-            return mapper.find_all()
+            return mapper.find_all(id)
 
+    #Speichern eines Users
     def save_user(self, user):
         with UserMapper() as mapper:
             mapper.update(user)
 
+    #löschen eines Users
     def delete_user(self, user):
         with UserMapper() as mapper:
             mapper.delete(user)
           
+
+    """
+    Recipe spezifische Methoden
+    
+    """
+
+    def create_recipe(self, recipe_id, groceries_id, groceries_quantity, groceries_unit_of_measurement):
+        #Benutzer erstellen
+        recipe = User()
+        recipe.set_first_name(first_name)
+        recipe.set_last_name(last_name)
+        recipe.set_nickname(nick_name)
+        recipe.set_google_id(google_user_id)
+        recipe.set_id(1) #default id, wird später in der Datenbank geupdatet
+
+        with RecipeMapper() as mapper:
+            return mapper.insert(recipe)
+
+    #Auslesen aller Rezepte
+    def get_all_recipe(self):
+        with UserMapper() as mapper:
+            return mapper.find_all()
+        
+    #Auslesen eines Rezepts
+    def get_all_recipe(self, id):
+        with UserMapper() as mapper:
+            return mapper.find_all(id)
+        
+    
+
 
 
 admintest = HalilsTaverneAdministration
