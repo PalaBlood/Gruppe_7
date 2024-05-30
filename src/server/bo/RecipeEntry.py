ab23a0eef5ecbed98b64bb83c2535d481f338ef6
PalaBlood <mischelmantalia3@gmail.com>
2024-05-30 13:28:47 +0200

    Kommentar hinzugefügt

--- src/server/bo/RecipeEntry.py
+++ src/server/bo/RecipeEntry.py
@@ -7,11 +7,13 @@ class RecipeEntry(FoodEntry):
         super().__init__()
         self.__recipe_id = None
 
-
     def get_recipe(self):
+
         return self.__recipe_id
 
     def set_recipe_id(self, recipe_id=Recipe()):
+        """RecipeEntry benötigt eine Referenz zu einem Rezept"""
+
         self.__recipe_id = recipe_id.get_id()
 
 
