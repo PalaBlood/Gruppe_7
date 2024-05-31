0b05b75e9bb39db79f53dfa7843957232c4f3e18
ts225hdm <ts225@hdm-stuttgart.de>
2024-05-31 13:08:28 +0200

    Update HalilsTaverneAdministration.py

--- src/server/HalilsTaverneAdministration.py
+++ src/server/HalilsTaverneAdministration.py
@@ -141,38 +141,6 @@ class HalilsTaverneAdministration(object):
             return mapper.find_all_entries()
 
 
-    def create_Grocerie(self, designation, unit_of_measurement, quantity):
-
-        groceries = Groceries()
-        groceries.set_designation(designation)
-        groceries.set_unit(unit_of_measurement)
-        groceries.set_quantity(quantity)
-        groceries.set_id(1)
-
-        with GroceriesMapper() as mapper:
-            return mapper.insert(groceries)
-        
-
-    def get_all_groceries(self):
-
-        with GroceriesMapper() as mapper:
-            mapper.find_all()
-
-    def get_groceries_by_id(self, groceries_id):
-
-        with GroceriesMapper() as mapper:
-            return mapper.find_by_id(groceries_id)
-        
-    def save_groceries(self, groceries):
-
-        with GroceriesMapper() as mapper:
-            return mapper.update(groceries)
-        
-    def delete_groceries(self, groceries):
-
-        with GroceriesMapper() as mapper:
-            mapper.delete(groceries)
-
     def delete_fridge_entry(self, fridge_entry):
 
         with FridgeMapper2() as mapper:
