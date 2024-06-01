7ac515061a7cc179af5a701c531628a42ba15e1b
ts225hdm <ts225@hdm-stuttgart.de>
2024-06-01 13:39:26 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
-from flask import flask
-from flas_restx import Api, Resource, fields
+from flask import Flask, jsonify
+from flask_restx import Api, Resource, fields
 from flask_cors import CORS
 
 from HalilsTaverneAdministration import HalilsTaverneAdministration
@@ -12,4 +12,33 @@ from User import User
 from SecurityDecorator import secured
 
 
-app = Flask
+app = Flask(__name__)
+
+
+CORS(app, resources={r"/api/":{"origins":"*"}})
+
+
+#alle pfade für ursprünge offen
+
+api = Api(app, version='1.0', title='SmartFridgeDemo',
+          description='Api für unsere SmartFridge')
+
+
+fridgesystem = api.namespace('fridge', description = "Funktionen der Fridge")
+
+bo = api.model('BusinessObject', {
+    'id': fields.Integer(attribute='_id', description= 'ID eines BO')
+})
+
+
+user = api.inherit('User', bo, {
+    'name': fields.String(),
+    'nick_name': fields.String(),
+    'first_name':fields.String(),
+    'last_name':fields.String()
+})
+
+
+Fridge = api.inherit('Fridge', bo, {
+    
+})
