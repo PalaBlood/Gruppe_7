4d07898f2c8a9dcd8511e144a1086087bb8756aa
ts225hdm <ts225@hdm-stuttgart.de>
2024-06-05 20:05:52 +0200

    Create FridgeAPI.js

---
+++ frontend/src/API/FridgeAPI.js
@@ -0,0 +1,42 @@
+import UserBO from "./UserBO";
+
+
+
+
+
+export default class FridgeAPI {
+
+    static #api = null
+
+    //python backend --> main.py, müssen wir noch reinschreiben, erzeugt sicherheit
+
+    #fridgeserverbaseurl = '/fridge';
+
+
+}
+
+
+
+//fridgeentry related
+
+
+
+//recipeentry related
+
+
+
+
+//fridge related
+
+
+
+//user related
+
+
+
+
+//recipe related
+
+
+
+//household related
