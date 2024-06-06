df1e683b0fdde5c0f708c62683209915852734a4
Tom Schönfeld <ts225@hdm-stuttgart.de>
2024-06-06 08:57:31 +0200

    Food Entry.js angelegt

---
+++ frontend/src/API/FoodEntry.js
@@ -0,0 +1,54 @@
+import BusinessObject from "./BusinessObject";
+
+
+export default class FoodEntry extends BusinessObject {
+
+    constructor (adesignation, aquantity, aunit) {
+        super();
+        this.designation = adesignation;
+        this.quantity = aquantity;
+        this.unit = aunit;
+    }
+
+    setdesignation (adesignation) {
+        this.designation = adesignation;
+    }
+
+    getdesignation () {
+        return this.designation;
+    }
+
+    setquantity(aquantity) {
+        this.quantity = aquantity
+    }
+
+    getquantity () {
+        return this.quantity
+    }
+
+    setunit (aunit) {
+        this.unit = aunit
+    }
+
+    getunit () {
+        return this.unit
+    }
+
+    static fromJSON(foodentries) {
+        let result = [];
+    
+        if (Array.isArray(foodentries)) {
+          users.forEach((u) => {
+            Object.setPrototypeOf(a, FoodEntry.prototype);
+            result.push(f);
+          })
+        } else {
+          // Es handelt sich offenbar um ein singuläres Objekt
+          let f = foodentries;
+          Object.setPrototypeOf(u, FoodEntry.prototype);
+          result.push(f);
+        }
+    
+        return result;
+      }
+}
