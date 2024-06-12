d5bd4c5ed0a592b534cdac97162e183b0e8f9e9a
ts225hdm <ts225@hdm-stuttgart.de>
2024-06-12 18:32:20 +0200

    added style and icons from mui

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -6,6 +6,8 @@ import ProfileDropDown from '../dialogs/ProfileDropDown';
 import UserList from '../UserList';
 import SettingsIcon from '@mui/icons-material/Settings';
 import MicrowaveIcon from '@mui/icons-material/Microwave';
+import HomeIcon from '@mui/icons-material/Home';
+import InfoIcon from '@mui/icons-material/Info';
 
 class Header extends Component {
   constructor(props) {
@@ -33,19 +35,39 @@ class Header extends Component {
     const { openUserList } = this.state;
 
     return (
-      <Paper variant='outlined'>
+      <Paper variant='outlined' style={{
+        borderRadius: '10px', 
+        overflow: 'hidden', 
+        boxShadow: '0 4px 10px rgba(0, 0, 0, 0.2)'
+      }}>
         <ProfileDropDown user={user} />
-        <Typography variant='h3' component='h1' align='center'>
-          CoolTech SmartFridges
+        <Typography variant='h3' component='h1' align='center' style={{
+          marginTop: '20px', 
+          marginBottom: '10px', 
+          fontWeight: 'bold'
+        }}>
+          HdMSmartFridge
         </Typography>
-        <Typography variant='h4' component='h2' align='center'>
-          Lebensmittel Rezepte Einkaufsliste Haushalt
+        <Typography variant='h4' component='h2' align='center' style={{
+          marginBottom: '20px'
+        }}>
+          
         </Typography>
         {user && (
-          <Tabs indicatorColor='primary' textColor='primary' centered value={this.state.tabindex} onChange={this.handleTabChange}>
+          <Tabs 
+            indicatorColor='primary' 
+            textColor='primary' 
+            centered 
+            value={this.state.tabindex} 
+            onChange={this.handleTabChange}
+            style={{
+              marginBottom: '20px'
+            }}
+          > 
+            <Tab icon={<HomeIcon/>} label='Home' component={RouterLink} to={process.env.PUBLIC_URL + '/home'}/>
             <Tab icon={<MicrowaveIcon />} label='Recipes' component={RouterLink} to={process.env.PUBLIC_URL + '/recipes'} />
             <Tab icon={<SettingsIcon />} label='Haushalt verwalten' onClick={this.toggleUserListModal} />
-            <Tab label='About' component={RouterLink} to={process.env.PUBLIC_URL + '/about'} />
+            <Tab icon={<InfoIcon/> }label='About' component={RouterLink} to={process.env.PUBLIC_URL + '/about'} />
           </Tabs>
         )}
         <Modal
@@ -53,11 +75,23 @@ class Header extends Component {
           onClose={this.toggleUserListModal}
           aria-labelledby="user-list-modal"
           aria-describedby="modal-modal-description"
+          style={{
+            display: 'flex', 
+            alignItems: 'center', 
+            justifyContent: 'center'
+          }}
         >
-          <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: 400, bgcolor: 'background.paper', boxShadow: 24, p: 4 }}>
-            <Typography id="user-list-modal-title" variant="h6" component="h2">
-              User List
-            </Typography>
+          <Box style={{
+            position: 'absolute', 
+            top: '50%', 
+            left: '50%', 
+            transform: 'translate(-50%, -50%)', 
+            width: 400, 
+            backgroundColor: 'white', 
+            boxShadow: '24px', 
+            padding: '20px', 
+            borderRadius: '10px'
+          }}>
             <UserList /> 
           </Box>
         </Modal>
--- frontend/src/components/pages/SignIn.js
+++ frontend/src/components/pages/SignIn.js
@@ -1,76 +1,71 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Button, Grid, Typography } from '@mui/material'; //Online Bib von der wir einige Komponenten verwenden können
-import backgroundImage from './smartfridge.jpg'; 
+import { Button, Grid, Typography, Box } from '@mui/material';
+import backgroundImage from './smartfridge.jpg';
 
-
-/** 
- * Renders a landing page for users who are not signed in. Provides a sign in button 
- * for using an existing google account to sign in. The component uses firebase to 
- * do redirect based signin process.
- * 
- * @see See Googles [firebase authentication](https://firebase.google.com/docs/web/setup)
- * @see See Googles [firebase API reference](https://firebase.google.com/docs/reference/js)
- * 
- */
 class SignIn extends Component {
-
-	/** 
-	 * Handles the click event of the sign in button an calls the prop onSignIn handler
-	 */
 	handleSignInButtonClicked = () => {
 		this.props.onSignIn();
 	}
 
-	/** Renders the sign in page, if user objext is null */
 	render() {
 		return (
 			<div style={{
 				backgroundImage: `url(${backgroundImage})`,
-                backgroundSize: 'cover', 
-                backgroundPosition: 'center',
-                backgroundRepeat: 'no-repeat',
-                width: '100vw', 
-                height: '100vh', 
-                position: 'fixed', 
-                top: 0,
-                left: 0,
-                zIndex: -1 
+				backgroundSize: 'cover',
+				backgroundPosition: 'center',
+				backgroundRepeat: 'no-repeat',
+				width: '100vw',
+				height: '100vh',
+				position: 'fixed',
+				top: 0,
+				left: 0,
+				zIndex: -1 
 			}}>
-			<div style={{
-				display: 'flex',
-				flexDirection:'column',
-				justifyContent:'center',
-				alignItems: 'center',
-				backgroundColor: 'lightblue',
-				marginRight: '600px',
-				marginLeft:'600px',
-				marginTop:'300px'
-			}}>
-				<Typography sx={{margin: 2}} align='center' variant='h6'>Wilkommen in unserer SmartFridge</Typography>
-				<Typography sx={{margin: 2}} align='center'>It appears, that you are not signed in.</Typography>
-				<Typography sx={{margin: 2}} align='center'>Sign in to your SmartFridge</Typography>
-				<Grid container justifyContent='center'>
-					<Grid item>
-						<Button variant='contained' color='primary' onClick={this.handleSignInButtonClicked}>
-							Sign in with Google
-						</Button>
-					</Grid>
-				</Grid>
-			</div>
+				<Box sx={{
+					position: 'absolute',
+					width: '100%',
+					height: '100%',
+					display: 'flex',
+					flexDirection: 'column',
+					justifyContent: 'center',
+					alignItems: 'center'
+				}}>
+					<Box sx={{
+						width: 'auto',
+						maxWidth: 360,
+						padding: 3,
+						borderRadius: 2,
+						boxShadow: 3,
+						backgroundColor: 'background.paper'
+					}}>
+						<Typography sx={{ margin: 2 }} variant='h5' align='center' fontWeight="bold">
+							Welcome to SmartFridge
+						</Typography>
+						<Typography sx={{ margin: 2 }} align='center'>
+							It appears that you are not signed in.
+						</Typography>
+						<Typography sx={{ margin: 2 }} align='center'>
+							Sign in to your SmartFridge
+						</Typography>
+						<Grid container justifyContent='center'>
+							<Button variant='contained' color='primary' sx={{ mt: 2 }} onClick={this.handleSignInButtonClicked}>
+								Sign in with Google
+							</Button>
+						</Grid>
+						<Grid container justifyContent='center' sx={{ mt: 1 }}>
+							<Typography variant="body2">
+								Don't have an account? <a href="https://www.google.com" target='_blank'>Sign up!</a>
+							</Typography>
+						</Grid>
+					</Box>
+				</Box>
 			</div>
 		);
 	}
 }
 
-
-
-//Hier müssen wir wahrscheinlich was was anpassen
-/** PropTypes */
 SignIn.propTypes = {
-	/** 
-	 * Handler function, which is called if the user wants to sign in.
-	 */
 	onSignIn: PropTypes.func.isRequired,
 }
 
