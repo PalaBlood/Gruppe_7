4462109b8257828c30568e159f0700456b7d72fc
ts225hdm <ts225@hdm-stuttgart.de>
2024-06-12 11:43:57 +0200

    Update UserListEntry.js

---
+++ frontend/src/components/UserListEntry.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Button, ListItem, ListItemSecondaryAction, Link, Typography } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SwapHoriz from '@mui/icons-material/SwapHoriz';
+import { Link as RouterLink } from 'react-router-dom';
+import { SmartFridgeAPI } from '../API/SmartFridgeAPI';
+import ContextErrorMessage from './dialogs/ContextErrorMessage';
+import LoadingProgress from './dialogs/LoadingProgress';
+import MoneyTransferDialog from './dialogs/MoneyTransferDialog';
