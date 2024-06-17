f7d0b0c28569a092a1a03057ce361b2edf40099c
PalaBlood <mischelmantalia3@gmail.com>
2024-06-17 23:49:40 +0200

    App.js angepasst

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import { Container, ThemeProvider, CssBaseline } from '@mui/material';
