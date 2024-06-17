9ffc90e5ff3b976d3223bc3e505e79fa087936a3
Tom Schönfeld <ts225@hdm-stuttgart.de>
2024-06-17 10:43:59 +0200

    Update Household.js

--- frontend/src/components/Household.js
+++ frontend/src/components/Household.js
@@ -1,1 +1,31 @@
+import React, { Component } from 'react'
 
+
+class Household extends Component() {
+
+    state = {
+       newHousehold: {household_name},
+       addHouseholdDialogOpen: false,
+       HouseholdDialogOpen: false,
+       error: null,
+       loading:true     
+    }
+
+    componentdidMount() {
+        this.addhouseholdorselecthousehold();
+    }
+
+    openaddHouseholdDialog () {
+        this.setState({addHouseholdDialogOpen : true})
+    }
+
+    closeaddHouseholdDialogOpen () {
+        this.setState({addHouseholdDialogOpen: false})
+    }
+
+    addhouseholdorselecthousehold() {
+
+    }
+
+    
+}
