8d2eb2430ea280748aad8e65d249e596e5dcaaca
Halil0927 <halilx2000@hotmail.de>
2024-07-01 20:45:55 +0200

    gcloud

---
+++ src/static/precache-manifest.295fc18370193ba72e2fb8b8b6a88fa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2ae8e02d3132ea808aef81336212f5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9eabf094df90e9c4216",
+    "url": "/static/js/2.bad088c3.chunk.js"
+  },
+  {
+    "revision": "d83ba3ea33eaa0f7f4cc4008f2925ace",
+    "url": "/static/js/2.bad088c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d66ba96ce7920a4eada",
+    "url": "/static/js/main.6f2e13b0.chunk.js"
+  },
+  {
+    "revision": "5b0cad7c0d4649957cc3",
+    "url": "/static/js/runtime-main.dab211f7.js"
+  },
+  {
+    "revision": "b39668d2a421b43246762d18005200d4",
+    "url": "/static/media/smartfridge.b39668d2.jpg"
+  }
+]);
